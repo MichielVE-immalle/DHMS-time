d61e854f288d8f571759eec87e6eaae4cfe15f7f
michiel.vaneester <michiel.vaneester@B209-13>
2017-05-30 13:42:30 +0200

    MetGlobaleVariabelen

--- Seconden to U.M.S/Program.cs
+++ Seconden to U.M.S/Program.cs
@@ -6,30 +6,47 @@ using System.Threading.Tasks;
 
 namespace Seconden_to_U.M.S
 {
-    struct HMSTime
-    {
-        public int Days;
-        public int Hours;
-        public int Mins;
-        public int Secs;
-    }
+    //struct HMSTime
+    //{
+    //    public int Days;
+    //    public int Hours;
+    //    public int Mins;
+    //    public int Secs;
+    //}
 
     class Program
     {
-        static HMSTime SecToHMS(int totalSeconds)
+        static int days;
+        static int hours;
+        static int mins;
+        static int secs;
+
+        static void SecToHoursMinsSecs(int totalSeconds)
         {
-            HMSTime a;
-            int rest;
+            int rest = 0;
 
-            a.Days = totalSeconds / (60 * 60 * 24);
+            days = totalSeconds / (60 * 60 * 24);
             rest = totalSeconds % (60 * 60 * 24);
-            a.Hours = rest / (60 * 60);
+            hours = rest / (60 * 60);
             rest = rest % (60 * 60);
-            a.Mins = rest / 60;
-            a.Secs = rest % 60;
-
-            return a;
+            mins = rest / 60;
+            secs = rest % 60;
         }
+
+        //static HMSTime SecToHMS(int totalSeconds)
+        //{
+        //    HMSTime a;
+        //    int rest;
+
+        //    a.Days = totalSeconds / (60 * 60 * 24);
+        //    rest = totalSeconds % (60 * 60 * 24);
+        //    a.Hours = rest / (60 * 60);
+        //    rest = rest % (60 * 60);
+        //    a.Mins = rest / 60;
+        //    a.Secs = rest % 60;
+
+        //    return a;
+        //}
 
         //static void SecTosHoursMinsSecs(int totalSeconds, out int days, out int hours, out int mins, out int secs)
         //{
@@ -45,23 +62,24 @@ namespace Seconden_to_U.M.S
 
         static void Main(string[] args)
         {
+            SecToHoursMinsSecs(61);
+
+            Console.WriteLine("{0} hours {1} minutes and {2} seconds", hours, mins, secs);
+
             //outVersie
 
             //int dagen = 0;
             //int uur = 0;
             //int minuten = 0;
             //int seconden = 0;
-
             //SecTosHoursMinsSecs(86401, out dagen, out uur, out minuten, out seconden);
-
             //Console.WriteLine("{0} dagen, {1} uur, {2} minuten, {3} seconden",dagen, uur, minuten, seconden);
 
 
             //MetMethodEnStruct
 
-            HMSTime time = SecToHMS(86401);
-
-            Console.WriteLine("{0} dagen, {1} uren, {2} minuten, {3} seconden", time.Days, time.Hours, time.Mins, time.Secs);
+            //HMSTime time = SecToHMS(86401);
+            //Console.WriteLine("{0} dagen, {1} uren, {2} minuten, {3} seconden", time.Days, time.Hours, time.Mins, time.Secs);
         }
     }
 }
