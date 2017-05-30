dcc93d155c99be03c592fb60aa31ee77008a988a
michiel.vaneester <michiel.vaneester@B209-13>
2017-05-30 13:44:21 +0200

    busy

--- Seconden to U.M.S/Program.cs
+++ Seconden to U.M.S/Program.cs
@@ -8,6 +8,18 @@ namespace Seconden_to_U.M.S
 {
     class Program
     {
+        static void SecTosHoursMinsSecs(int totalSeconds, out int days, out int hours, out int mins, out int secs)
+        {
+            int rest = 0;
+
+            days = totalSeconds / (60 * 60 * 24);
+            rest = totalSeconds % (60 * 60 * 24);
+            hours = rest / (60 * 60);
+            rest = rest % (60 * 60);
+            mins = rest / 60;
+            secs = rest % 60;
+        }
+
         static void Main(string[] args)
         {
         }
